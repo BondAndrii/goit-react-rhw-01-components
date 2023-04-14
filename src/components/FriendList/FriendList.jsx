c635d5b42f5283d4dd3d28465dc7ef3e0f94ca3b
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-04-14 22:07:56 +0300

    add inline style

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,10 +4,24 @@ export const FriendList = ({friends}) => {
     
     return (
         <ul>
-            {friends.map(({id, name, avatar, isOnline}) => {
+            {friends.map(({ id, name, avatar, isOnline }) => {
+                const bgcolor = () => {
+                    return (
+                        isOnline ? "red" : "green"
+                    )
+                }
                 return (
                     <li key={id}>
-                        <span></span>
+                        <span style={
+                            {
+                                display:"inline-block",
+                                width: "20px",
+                                height: "20px",
+                                borderRadius: "50%",
+                                 backgroundColor: bgcolor(),
+                            }
+                                }
+                           ></span>
                         <img src={avatar} alt={name} />
                         <p>{name}</p>
                         
